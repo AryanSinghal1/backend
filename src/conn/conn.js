d4b174312a55ab0bba02e967dd365096ce3a2aa2
Aryan <Aryans8791@gmail.com>
2022-07-31 10:52:53 +0530

    interval

--- src/conn/conn.js
+++ src/conn/conn.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
-mongoose.connect(process.env.MONGODB_URL).then(()=>{
+mongoose.connect("mongodb+srv://TeamNewshell:eovXKgDbc4ZEtNzs@cluster0.wf0qiyf.mongodb.net/?retryWrites=true&w=majority",{
+    useNewUrlParser: true,
+        useUnifiedTopology: true
+}).then(()=>{
     console.log("Connection Successful");
 }).catch((e)=>{
     console.log(e);
